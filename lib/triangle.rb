863f230828a1d8575f845ac165abab855aebdca7
Allison MacQueen <allimacq@gmail.com>
2020-11-18 22:08:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
  attr_accessor :left_side, :right_side, :bottom
  
- def initialize
+ def initialize(left_side:, right_side:, bottom:)
    @left_side = left_side
    @right_side = right_side
    @bottom = bottom
