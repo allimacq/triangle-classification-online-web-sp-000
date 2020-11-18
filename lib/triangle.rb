024d703d645c102e5b9f16c77d28968f5030983e
Allison MacQueen <allimacq@gmail.com>
2020-11-18 22:05:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :left_side, :right_side, :bottom
+  
+  def initialize(left_side:, right_side:, bottom:)
+    @left_side = left_side
+    @right_side = right_side
+    @bottom = bottom
+
+  end
+  
+  def kind
+    #if equilateral
+   if @left_side == @right_side && @right_side == @bottom && @left_side == @bottom
+     :equilateral
+    #if isosceles
+    #elsif @left_side == @right_side || @left_side == @bottom || @right_side == @bottom
+     # :isosceles
+    #elsif @left_side != @right_side && @right_side != @bottom && @left_side != @bottom
+     # :scalene
+    end
+  end
+    
+  #class TriangleError < StandardError
+    #if left_side < 0
+  #end
 end
